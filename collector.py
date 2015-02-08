32c4a1bb3d60358025de4ba131bdba203331d63e
aiskov <hasado@yandex.ru>
2015-02-08 22:51:43 +0100

    Update

--- collector.py
+++ collector.py
@@ -1,45 +1,8 @@
-import os
-from subprocess import Popen, PIPE
-import tarfile
+from os import listdir, remove
+from shutil import rmtree
+from utils import load_json, call, check_skip, create_dir, extract, move
 import json
-import shutil
 import re
-
-
-def call(cmd):
-    return Popen([cmd], stdout=PIPE, shell=True).communicate()[0]
-
-
-def move(file, origin, destination):
-    if not file:
-        return
-
-    if not isinstance(file, basestring):
-        for entry in file:
-            move(entry, origin, destination)
-
-        return
-
-    destination_path = '%s/%s' % (destination, re.sub('(\./|bin/|dist/)', '', file))
-    origin_path = '%s/%s' % (origin, re.sub('(\./)', '', file))
-
-    shutil.move(origin_path, destination_path)
-
-
-def load_json(conf):
-    json_data = open(conf)
-    data = json.load(json_data)
-    json_data.close()
-
-    return data
-
-
-def check_skip(text, skips):
-    for word in skips:
-        if word in text:
-            return True
-
-    return False
 
 config = load_json('cdn-config.json')
 
@@ -48,14 +11,13 @@ print 'Rebuild CDN collection.'
 for target in config['targets']:
     print 'Collect %s libraries.' % target
 
+    create_dir(target)
+
     lib_info = call("bower info %s -j" % target)
-    print lib_info
-    lib_info = lib_info[lib_info.rfind('}]'):]
+    if not lib_info:
+        print 'Cannot collect information about library'
+        break
 
-    print "XXXXXXXXX" + str(lib_info.rfind('}\]'))
-    print lib_info
-
-    break
     versions = [
         version for version in json.loads(lib_info)['versions']
         if not check_skip(version, config['skipWords'])
@@ -64,19 +26,20 @@ for target in config['targets']:
     for version in versions:
         print 'Version found %s - %s.' % (target, version)
 
-        if not os.path.isdir(target):
-            os.mkdir(target)
-
         directory = "%s/%s" % (target, version)
-        if os.path.isdir(directory):
-            if os.listdir(directory):
-                print 'Skip version, directory already exists %s/%s' % (target, version)
-                continue
-        else:
-            os.mkdir("%s/%s" % (target, version))
+        if not create_dir(directory) and listdir(directory):
+            print 'Skip version, directory already exists %s/%s and not empty' % (target, version)
+            continue
 
-        info = call("bower info %s#%s -j" % (target, version))
-        info = json.loads(info[info.rfind('}]') + 2:])
+        info = call("bower info %s#%s -j" % (target, version), True)
+
+        if info[0]:
+            files = json.loads(info[0])
+            files = files.get('main', files.get('scripts'))
+        else:
+            files = None
+
+        info = json.loads(info[1])
 
         link = None
         for entry in info:
@@ -85,19 +48,24 @@ for target in config['targets']:
                 break
 
         if not link:
-            link = 'http/%s/archive/%s.tar.gz' % (
-                re.finditer('git://(.c*)\.git', info[0]["message"]).next().groups()[0], version
-            )
+            tag_name = None
+
+            if 'data' in info[0] and 'pkgMeta' in info[0]['data']:
+                if '_resolution' in info[0]['data']['pkgMeta'] and 'tag' in info[0]['data']['pkgMeta']['_resolution']:
+                    tag_name = info[0]['data']['pkgMeta']['_resolution']['tag']
+
+            tag_name = version if not tag_name else tag_name
+
+            link = '%s/archive/%s.tar.gz' % (re.sub('(git://|\.git.*)', '', info[0]["message"]), tag_name)
 
         print call('wget --directory-prefix="%s" "%s"' % (directory, link))
 
-        #if os.listdir(directory):
-        #    archive = "%s/%s" % (directory, os.listdir(directory)[0])
-        #    tfile = tarfile.open(archive, 'r:gz')
-        #    tfile.extractall(directory)
-        #    os.remove(archive)
+        if listdir(directory):
+            archive = "%s/%s" % (directory, listdir(directory)[0])
+            extract(archive, directory)
+            remove(archive)
 
-        #    location = "%s/%s" % (directory, os.listdir(directory)[0])
-        #    move(info.get('min', info.get('scripts')), location, directory)
+            location = "%s/%s" % (directory, listdir(directory)[0])
+            move(files if files else listdir(location), location, directory)
 
-        #    shutil.rmtree(location)
+            rmtree(location)
---
+++ utils.py
@@ -0,0 +1,76 @@
+from subprocess import Popen, PIPE
+import json
+import shutil
+import re
+import os
+import tarfile
+
+
+def extract(archive, directory):
+    tfile = tarfile.open(archive, 'r:gz')
+    tfile.extractall(directory)
+
+
+def create_dir(target):
+    if not os.path.isdir(target):
+        os.mkdir(target)
+        print 'Directory created: %s' % target
+
+        return True
+
+    return False
+
+
+def call(cmd, err=False):
+    result = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True).communicate()
+
+    if err:
+        return result
+
+    if not result[0] and result[1]:
+        print result[1]
+        return None
+
+    return result[0]
+
+
+def move(target, origin, destination):
+    if not target:
+        return
+
+    if not isinstance(target, basestring):
+        for entry in target:
+            move(entry, origin, destination)
+
+        return
+
+    target = target.replace('/*', '')
+
+    destination_path = '%s/%s' % (destination, re.sub('(\./|bin/|dist/|release/)', '', target))
+    origin_path = '%s/%s' % (origin, re.sub('(\./)', '', target))
+
+    try:
+        os.makedirs(os.path.dirname(destination_path))
+    except os.error:
+        pass
+
+    try:
+        shutil.move(origin_path, destination_path)
+    except Exception as error:
+        print error.message
+
+
+def load_json(conf):
+    json_data = open(conf)
+    data = json.load(json_data)
+    json_data.close()
+
+    return data
+
+
+def check_skip(text, skips):
+    for word in skips:
+        if word in text:
+            return True
+
+    return False

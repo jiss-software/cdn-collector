7d4eb4219f97842bbdf41b444810adcaafaea037
Anton Iškov <iskov.anton@yandex.com>
2015-03-02 21:52:43 +0100

    Update collector.py

--- collector.py
+++ collector.py
@@ -13,7 +13,7 @@ for target in config['targets']:
 
     create_dir(target)
 
-    lib_info = call("bower info %s -j" % target)
+    lib_info = call("bower info %s -j --allow-root" % target)
     if not lib_info:
         print 'Cannot collect information about library'
         break
@@ -26,46 +26,15 @@ for target in config['targets']:
     for version in versions:
         print 'Version found %s - %s.' % (target, version)
 
-        directory = "%s/%s" % (target, version)
-        if not create_dir(directory) and listdir(directory):
+        target_directory = "%s/%s" % (target, version)
+        if not create_dir(target_directory) and listdir(target_directory):
             print 'Skip version, directory already exists %s/%s and not empty' % (target, version)
             continue
 
-        info = call("bower info %s#%s -j" % (target, version), True)
+        info = call("bower install %s#%s -j--allow-root" % (target, version), True)
 
-        if info[0]:
-            files = json.loads(info[0])
-            files = files.get('main', files.get('scripts'))
-        else:
-            files = None
-
-        info = json.loads(info[1])
-
-        link = None
-        for entry in info:
-            if entry["id"] is "download":
-                link = entry["message"]
-                break
-
-        if not link:
-            tag_name = None
-
-            if 'data' in info[0] and 'pkgMeta' in info[0]['data']:
-                if '_resolution' in info[0]['data']['pkgMeta'] and 'tag' in info[0]['data']['pkgMeta']['_resolution']:
-                    tag_name = info[0]['data']['pkgMeta']['_resolution']['tag']
-
-            tag_name = version if not tag_name else tag_name
-
-            link = '%s/archive/%s.tar.gz' % (re.sub('(git://|\.git.*)', '', info[0]["message"]), tag_name)
-
-        print call('wget --directory-prefix="%s" "%s"' % (directory, link))
-
-        if listdir(directory):
-            archive = "%s/%s" % (directory, listdir(directory)[0])
-            extract(archive, directory)
-            remove(archive)
-
-            location = "%s/%s" % (directory, listdir(directory)[0])
-            move(files if files else listdir(location), location, directory)
-
-            rmtree(location)
+        tmp_directory = 'tmp'
+        if listdir('tmp'):
+            tmp_directory = "%s/%s" % (tmp_directory, listdir(tmp_directory)[0])
+            move(listdir(tmp_directory), tmp_directory, directory)
+            rmtree(tmp_directory)
